ac04f6a9087ddc208ae3e3aa7d3a4cf65e13229e
ashmisn <144801194+ashmisn@users.noreply.github.com>
2025-09-08 19:05:27 +0530

    small change

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
-
+//const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'REACT_APP_API_BASE_URL';
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
